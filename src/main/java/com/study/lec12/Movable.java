189b466a67428d437132742f9ae36eea66e5b97e
jay-so <merry9504@naver.com>
2024-04-04 09:33:04 +0900

    build: 코틀린 프로젝트 설정

---
+++ src/main/java/com/study/lec12/Movable.java
@@ -0,0 +1,9 @@
+package com.study.lec12;
+
+public interface Movable {
+
+  void move();
+
+  void fly();
+
+}
---
+++ src/main/java/com/study/lec17/FruitFilter.java
@@ -0,0 +1,8 @@
+package com.study.lec17;
+
+public interface FruitFilter {
+
+  boolean isSelected(Fruit fruit);
+
+}
+
